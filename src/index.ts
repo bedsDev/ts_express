da7a7cb337d2cb5380e74d45c81a8192d5035832
dennis wu <nets.beds@gmail.com>
2025-03-17 21:10:07 +0000

    update to response url

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,22 @@ import { log } from "@utils/logger";
 const app: Express = express();
 const PORT: string | number = process.env.PORT || 5000; 
 
-app.get("/", (req: Request, res: Response) => {
-  log("GET /");
+app.get("/*", (req: Request, res: Response) => {
+  log("GET /*");
+  log(JSON.stringify(req.hostname));
+  log(JSON.stringify(req.ip));
   getUser();
-  res.send("Hello World: Express and TypeScript");
+  res.send(`<h1>Hello World: Express and TypeScript</h1>
+    <p><b>Hostname</b>: ${req.hostname}</p>  
+    <p><b>app</b>: ${req.app}</p>  
+    <p><b>baseUrl</b>: ${req.baseUrl}</p>  
+    <p><b>body</b>: ${req.body}</p>  
+    <p><b>params</b>: ${JSON.stringify(req.params)}</p>  
+    <p><b>path</b>: ${req.path}</p>  
+    <p><b>query</b>: ${JSON.stringify(req.query)}</p>  
+    <p><b>route</b>: ${JSON.stringify(req.route)}</p>
+    <p><b>url</b>: ${JSON.stringify(req.url)}</p>
+    `);
 });
 
 app.listen(PORT, () => {
