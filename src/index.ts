94874cfacf306ece482f76c329519ac4011157f8
dennis wu <nets.beds@gmail.com>
2025-03-20 20:21:23 +0000

    using environmental variables

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,26 @@ dotenv.config();import { getUser } from "@services/userService";
 import { log } from "@utils/logger";
 
 const app: Express = express();
-const PORT: string | number = process.env.PORT || 5000; 
+const PORT: string | number = process.env.SERVER_PORT || 5000; 
 
 app.get("/*", (req: Request, res: Response) => {
   log("GET /*");
   log(JSON.stringify(req.hostname));
   log(JSON.stringify(req.ip));
   getUser();
-  res.send(`<h1>Hello World: Express and TypeScript!</h1>
-    <p><b>Hostname</b>: ${req.hostname}</p>  
-    <p><b>app</b>: ${req.app}</p>  
-    <p><b>baseUrl</b>: ${req.baseUrl}</p>  
-    <p><b>body</b>: ${req.body}</p>  
-    <p><b>params</b>: ${JSON.stringify(req.params)}</p>  
-    <p><b>path</b>: ${req.path}</p>  
-    <p><b>query</b>: ${JSON.stringify(req.query)}</p>  
-    <p><b>route</b>: ${JSON.stringify(req.route)}</p>
-    <p><b>url</b>: ${JSON.stringify(req.url)}</p>
-    `);
+  const headers = req.headers;
+  res.send(headers);
+  // res.send(`<h1>Hello World: Express and TypeScript!</h1>
+  //   <p><b>Hostname</b>: ${req.hostname}</p>  
+  //   <p><b>app</b>: ${req.app}</p>  
+  //   <p><b>baseUrl</b>: ${req.baseUrl}</p>  
+  //   <p><b>body</b>: ${req.body}</p>  
+  //   <p><b>params</b>: ${JSON.stringify(req.params)}</p>  
+  //   <p><b>path</b>: ${req.path}</p>  
+  //   <p><b>query</b>: ${JSON.stringify(req.query)}</p>  
+  //   <p><b>route</b>: ${JSON.stringify(req.route)}</p>
+  //   <p><b>url</b>: ${JSON.stringify(req.url)}</p>
+  //   `);
 });
 
 app.listen(PORT, () => {
